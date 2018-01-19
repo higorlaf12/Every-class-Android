75b9fe769816b3cd801a9dbf9c8e7cca20b2b58c
higorlaf12 <higor.freitas@neppoadm.com.br>
2018-01-19 09:53:45 -0200

    Projeto adivinha inicio

---
+++ Adivinha/app/src/main/java/com/example/aulasandroid/adivinha/adivinhaMain.java
@@ -0,0 +1,13 @@
+package com.example.aulasandroid.adivinha;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class adivinhaMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adivinha_main);
+    }
+}

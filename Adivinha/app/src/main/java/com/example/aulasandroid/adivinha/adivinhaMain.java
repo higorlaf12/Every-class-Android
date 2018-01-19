4961e3f0564d4363f9598e960a8ff674f6d93ed2
higorlaf12 <higor.freitas@neppoadm.com.br>
2018-01-19 10:32:38 -0200

    Meu primeiro app completo. Seção 7

--- Adivinha/app/src/main/java/com/example/aulasandroid/adivinha/adivinhaMain.java
+++ Adivinha/app/src/main/java/com/example/aulasandroid/adivinha/adivinhaMain.java
@@ -1,13 +1,39 @@
 package com.example.aulasandroid.adivinha;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class adivinhaMain extends AppCompatActivity {
+
+    private Button botaoJogar;
+    private TextView textoResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adivinha_main);
+
+        botaoJogar = findViewById(R.id.botaoJogarId);
+        textoResultado = findViewById(R.id.resultadoDoID);
+
+       botaoJogar.setOnClickListener(new View.OnClickListener() {
+           @SuppressLint("SetTextI18n")
+           @Override
+           public void onClick(View v) {
+
+               Random random = new Random();
+
+               int numeroAleatorio = random.nextInt(10);
+
+
+               textoResultado.setText("Número escolhido: " + numeroAleatorio);
+           }
+       });
     }
 }
